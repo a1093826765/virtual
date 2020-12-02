3a0d666d46556f7c59546db2974ebc1d007ff3cb
Novmeber <1093826765@qq.com>
2020-12-02 11:56:21 +0800

    更新异常处理器

--- src/main/java/com/nov/virtual/exceptions/GloabllExceptionHandler.java
+++ src/main/java/com/nov/virtual/exceptions/GloabllExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.nov.virtual.exceptions;
 
+import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import com.nov.virtual.utils.pojo.ResultCode;
 import com.nov.virtual.utils.pojo.ResultUtils;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +14,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @ControllerAdvice
 public class GloabllExceptionHandler {
+
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public Object handlerException(Exception e){
         e.printStackTrace();
         return new ResultUtils(ResultCode.SYSTEM_ERROR);
     }
+
+    /**
+     * 参数为空异常
+     * @param e
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public Object handlerException(HttpMessageNotReadableException e){
+        return new ResultUtils(ResultCode.PARAM_IS_BLANK);
+    }
+
 }
 

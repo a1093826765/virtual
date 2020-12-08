1ee86b0ffe0deedb78c564528452af606568fb9e
Novmeber <1093826765@qq.com>
2020-12-08 09:37:58 +0800

    更新socket接口

--- src/main/java/com/nov/virtual/controller/web/SocketController.java
+++ src/main/java/com/nov/virtual/controller/web/SocketController.java
@@ -1,12 +1,7 @@
-package com.nov.virtual.controller;
+package com.nov.virtual.controller.web;
 
-import com.nov.virtual.websocket.WebSocketServer;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**

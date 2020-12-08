6d535f69b9f5b44e3b8f7e450bba273a87ad4efd
Novmeber <1093826765@qq.com>
2020-12-08 09:34:31 +0800

    添加订单类型接口

---
+++ src/main/java/com/nov/virtual/vo/OrderTypeVo.java
@@ -0,0 +1,20 @@
+package com.nov.virtual.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 用户订单类型参数
+ * @author november
+ */
+@Data
+@ApiModel(value = "用户订单类型参数",description="/api/admin/orderType/**")
+public class OrderTypeVo {
+
+    @ApiModelProperty(value="订单类型Id",name="orderTypeId",required=false)
+    private Integer orderTypeId;
+
+    @ApiModelProperty(value="订单类型",name="orderTypeName",required=false)
+    private String orderTypeName;
+}

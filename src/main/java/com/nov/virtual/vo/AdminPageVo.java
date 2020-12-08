7860f583a9893c2980ee484deabbb0af7a78b166
Novmeber <1093826765@qq.com>
2020-12-08 09:32:17 +0800

    修改后端分页参数

---
+++ src/main/java/com/nov/virtual/vo/AdminPageVo.java
@@ -0,0 +1,26 @@
+package com.nov.virtual.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NonNull;
+
+/**
+ * 分页查询(带userId)
+ * @author november
+ */
+@Data
+@ApiModel(value = "分页查询参数(带userId)",description="/api/admin/**/query")
+public class AdminPageVo {
+    @ApiModelProperty(value="页数",name="page",required=true)
+    @NonNull
+    private Integer page;
+
+    @ApiModelProperty(value="数据行数",name="limit",required=true)
+    @NonNull
+    private Integer limit;
+
+    @ApiModelProperty(value="用户Id",name="userId",required=true)
+    @NonNull
+    private Integer userId;
+}

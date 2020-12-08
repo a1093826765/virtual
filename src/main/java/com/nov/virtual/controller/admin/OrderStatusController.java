1d3b68122f3b2fcc8fe6f60be84869f59afea0fb
Novmeber <1093826765@qq.com>
2020-12-08 09:33:53 +0800

    添加订单状态接口

---
+++ src/main/java/com/nov/virtual/controller/admin/OrderStatusController.java
@@ -0,0 +1,80 @@
+package com.nov.virtual.controller.admin;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.nov.virtual.sql.model.OrderStatus;
+import com.nov.virtual.sql.model.OrderStatusExample;
+import com.nov.virtual.sql.model.OrderStatusKey;
+import com.nov.virtual.sql.service.OrderStatusService;
+import com.nov.virtual.utils.pojo.ResultCode;
+import com.nov.virtual.utils.pojo.ResultUtils;
+import com.nov.virtual.vo.OrderStatusVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 订单类型接口
+ * @author november
+ */
+@Api(value = "订单类型Controller",tags = {"订单类型接口"})
+@RestController
+@RequestMapping(value = "/api/admin/orderStatus",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+public class OrderStatusController {
+    @Autowired
+    OrderStatusService orderStatusService;
+
+    @ApiOperation(value = "订单类型查询",notes = "此接口查询订单类型信息")
+    @GetMapping("/query")
+    public ResultUtils queryCurrency(){
+        List<OrderStatus> orderStatusList = orderStatusService.getOrderStatusByExample(new OrderStatusExample());
+        JSONArray jsonArray=new JSONArray();
+        for(int i=0;i<orderStatusList.size();i++){
+            OrderStatus orderStatus=orderStatusList.get(i);
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("orderStatusId",orderStatus.getOrderstatusid());
+            jsonObject.put("orderStatusName",orderStatus.getOrderstatusname());
+            jsonArray.add(jsonObject);
+        }
+        return ResultUtils.success(jsonArray);
+    }
+
+    @ApiOperation(value = "添加订单类型",notes = "此接口添加订单类型信息")
+    @PostMapping("/insert")
+    public ResultUtils insertCurrency(@Validated @RequestBody OrderStatusVo orderStatusVo){
+        OrderStatus orderStatus =new OrderStatus();
+        orderStatus.setOrderstatusname(orderStatusVo.getOrderStatusName());
+        if(orderStatusService.save(orderStatus)==1){
+            return ResultUtils.success();
+        }
+        return ResultUtils.fail(ResultCode.SYSTEM_ERROR);
+    }
+
+    @ApiOperation(value = "删除订单类型",notes = "此接口删除订单类型信息")
+    @PostMapping("/delete")
+    public ResultUtils deleteCurrency(@Validated @RequestBody OrderStatusVo orderStatusVo){
+        OrderStatusKey orderStatusKey=new OrderStatusKey();
+        orderStatusKey.setOrderstatusid(orderStatusVo.getOrderStatusId());
+        if(orderStatusService.deleteByKey(orderStatusKey)==1){
+            return ResultUtils.success();
+        }
+        return ResultUtils.fail(ResultCode.SYSTEM_ERROR);
+    }
+
+    @ApiOperation(value = "修改订单类型",notes = "此接口修改订单类型信息")
+    @PostMapping("/update")
+    public ResultUtils updateCurrency(@Validated @RequestBody OrderStatusVo orderStatusVo){
+        OrderStatus orderStatus =new OrderStatus();
+        orderStatus.setOrderstatusid(orderStatusVo.getOrderStatusId());
+        orderStatus.setOrderstatusname(orderStatusVo.getOrderStatusName());
+        if(orderStatusService.updateByKey(orderStatus)==1){
+            return ResultUtils.success();
+        }
+        return ResultUtils.fail(ResultCode.SYSTEM_ERROR);
+    }
+}
---
+++ src/main/java/com/nov/virtual/vo/OrderStatusVo.java
@@ -0,0 +1,20 @@
+package com.nov.virtual.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 用户订单状态参数
+ * @author november
+ */
+@Data
+@ApiModel(value = "用户订单状态参数",description="/api/admin/orderStatus/**")
+public class OrderStatusVo {
+
+    @ApiModelProperty(value="订单状态Id",name="orderStatusId",required=false)
+    private Integer orderStatusId;
+
+    @ApiModelProperty(value="订单状态",name="orderStatusName",required=false)
+    private String orderStatusName;
+}
